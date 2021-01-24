9c4781011f2fa9ca98d6af369ab99d36a703c655
SchooKish <kishkola@gmail.com>
2021-01-24 20:49:42 +0300

    add contacts page

--- dance/JS/slider.js
+++ dance/JS/slider.js
@@ -1,10 +1,10 @@
 $(".photo").slick({
-  dots: true,
+  dots: false,
   arrows: false,
   infinite: true,
   speed: 500,
   fade: true,
   cssEase: "linear",
   autoplay: true,
-  autoplaySpeed: 4000,
+  autoplaySpeed: 3000,
 });
